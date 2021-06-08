243c5b1a468a9d97be5b3ffc23fc84439fd8b5dd
turbanisch <fetu@gmx.de>
2021-06-08 23:27:32 +0200

    rename dataset

--- R/clean_wdi.R
+++ R/clean_wdi.R
@@ -2,10 +2,10 @@ clean_wdi <- function(df) {
   # Rename variable
   df <- df %>%
     dplyr::rename("fert_consumption" = AG.CON.FERT.ZS)
-  
+
   # add iso3 codes
-  df <- df %>% 
+  df1 <- df %>% 
     mutate(iso3c = countrycode(iso2c, "iso2c", "iso3c"))
   
-  return(df)
+  return(df1)
 }
