b09df4e245b94df3cd7d6f196edf15656b8256aa
turbanisch <fetu@gmx.de>
2021-06-11 11:34:51 +0200

    remove commented "verbose" version
    verbose version has a separate branch now

--- _targets.R
+++ _targets.R
@@ -19,15 +19,3 @@ list(
   tar_target(final, merge_data(oecd, wdi), format = "file"),
   tar_render(report, "Report.Rmd")
 )
-# not using external functions
-# list(
-#   tar_target(wdi_raw, WDI(indicator = "AG.CON.FERT.ZS") %>% as_tibble()),
-#   tar_target(oecd_path, "data/raw/oecd.csv", format = "file"),
-#   tar_target(wdi, wdi_raw %>%
-#                rename("fert_consumption" = AG.CON.FERT.ZS) %>% 
-#                mutate(iso3c = countrycode(iso2c, "iso2c", "iso3c"))),
-#   tar_target(oecd, read_csv(oecd_path) %>% 
-#                mutate(iso3c = countrycode(oecd_name, "country.name.en", "iso3c"))),
-#   tar_target(final, merge_data(oecd_cleaned, wdi_cleaned), format = "file"),
-#   tar_render(report, "Report.Rmd")
-# )
