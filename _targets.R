ef3cfd81246fcfe94a63732b29b1140fce17bc66
turbanisch <fetu@gmx.de>
2021-06-11 11:34:03 +0200

    do not use external functions

--- _targets.R
+++ _targets.R
@@ -12,22 +12,23 @@ tar_option_set(packages = c("tidyverse",
 
 # End this file with a list of target objects.
 list(
-  tar_target(wdi_raw, get_wdi()),
-  tar_target(oecd_path, "data/raw/oecd.csv", format = "file"),
-  tar_target(wdi, clean_wdi(wdi_raw)),
-  tar_target(oecd, clean_oecd(oecd_path)),
-  tar_target(final, merge_data(oecd, wdi), format = "file"),
-  tar_render(report, "Report.Rmd")
+  tar_target(wdi_raw,
+             WDI(indicator = "AG.CON.FERT.ZS") %>% as_tibble()),
+  tar_target(oecd_path,
+             "data/raw/oecd.csv",
+             format = "file"),
+  tar_target(wdi,
+             wdi_raw %>% 
+               rename("fert_consumption" = AG.CON.FERT.ZS) %>% 
+               mutate(iso3c = countrycode(iso2c, "iso2c", "iso3c"))
+  ),
+  tar_target(oecd,
+             read_csv(oecd_path) %>%
+               mutate(iso3c = countrycode(oecd_name, "country.name.en", "iso3c"))
+             ),
+  tar_target(final,
+             merge_data(oecd, wdi),
+             format = "file"),
+  tar_render(report,
+             "Report.Rmd")
 )
-# not using external functions
-# list(
-#   tar_target(wdi_raw, WDI(indicator = "AG.CON.FERT.ZS") %>% as_tibble()),
-#   tar_target(oecd_path, "data/raw/oecd.csv", format = "file"),
-#   tar_target(wdi, wdi_raw %>%
-#                rename("fert_consumption" = AG.CON.FERT.ZS) %>% 
-#                mutate(iso3c = countrycode(iso2c, "iso2c", "iso3c"))),
-#   tar_target(oecd, read_csv(oecd_path) %>% 
-#                mutate(iso3c = countrycode(oecd_name, "country.name.en", "iso3c"))),
-#   tar_target(final, merge_data(oecd_cleaned, wdi_cleaned), format = "file"),
-#   tar_render(report, "Report.Rmd")
-# )
