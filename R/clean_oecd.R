d8ec28e2e5d2db7cf1df31cc5f40ae77e61da9a1
turbanisch <fetu@gmx.de>
2021-06-11 11:23:52 +0200

    reverse (irrelevant) name change

--- R/clean_oecd.R
+++ R/clean_oecd.R
@@ -1,10 +1,10 @@
 clean_oecd <- function(path) {
   # read data
-  df1 <- read_csv(path)
+  df <- read_csv(path)
   
   # add iso3 codes
-  df1 <- df1 %>% 
+  df <- df %>% 
     mutate(iso3c = countrycode(oecd_name, "country.name.en", "iso3c"))
   
-  return(df1)
+  return(df)
 }
